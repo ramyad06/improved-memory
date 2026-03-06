012a4ba027e515fcb419c18da59bf69a1b812ad1
ramyad06 <ramyaram0603@gmail.com>
2026-03-06 11:18:43 +0530

    db added

---
+++ database.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./todos.db' #address of the database
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False}) #actual connection & allows multiple parts of your app to use the same connection
+
+SessionLocal = sessionmaker(autocommit = False, autoflush=False, bind=engine) #a class that CREATES sessions whenever you call SessionLocal()
+
+Base = declarative_base() #parent class that all your database MODELS will inherit from.
+

e1fbcc2f4f7a6a2d6469c05d553f36e2cd25b520
ramyad06 <ramyaram0603@gmail.com>
2026-03-09 23:05:04 +0530

    migrated to postgresDB

--- database/db.py
+++ database/db.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db' #address of the database
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:Indium%40123@localhost/TodoApplicationDatabase' #address of the database
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False}) #actual connection & allows multiple parts of your app to use the same connection
+engine = create_engine(SQLALCHEMY_DATABASE_URL) #actual connection & allows multiple parts of your app to use the same connection
 
 SessionLocal = sessionmaker(autocommit = False, autoflush=False, bind=engine) #a class that CREATES sessions whenever you call SessionLocal()
 
--- routers/Users.py
+++ routers/Users.py
@@ -20,6 +20,10 @@ def get_db():
     finally:
         db.close()
 
+class UserVerification(BaseModel):
+    password: str
+    new_password:str = Field(min_length=6)
+
 db_dependency = Annotated[Session, Depends(get_db)]
 user_dependency = Annotated[dict, Depends(get_current_user)]
 bcrypt_context = CryptContext(schemes=['bcrypt'],deprecated='auto')
@@ -29,3 +33,17 @@ async def get_user(user: user_dependency, db:db_dependency):
     if user is None:
         raise HTTPException(status_code=401, detail='Authentication Failed')
     return db.query(Users).filter(user.id == user.get('id')).first()
+
+@router.put("/password", status_code=status.HTTP_204_NO_CONTENT)
+async def change_password(user:user_dependency, db:db_dependency, User_verification: UserVerification):
+    if user is None:
+        raise HTTPException(status_code=401, detail='Authentication Failed')
+    user_model = db.query(Users).filter(Users.id == user.get('id')).first()
+
+    if not bcrypt_context.verify(User_verification.password, user_model.hashed_password):
+        raise HTTPException(status_code=401, detail='Error on password change')
+    
+    user_model.hashed_password = bcrypt_context.hash(User_verification.new_password)
+
+    db.add(user_model)
+    db.commit()
